8f4eb190dd5674a4e8d72093fba6ac465a097e4f
gchuaunc <gchua@unc.edu>
2023-10-10 09:24:47 -0400

    feat: add bike pictures

--- src/app/interests/bikefix/bikefix.component.ts
+++ src/app/interests/bikefix/bikefix.component.ts
@@ -6,5 +6,49 @@ import { Component } from '@angular/core';
   styleUrls: ['./bikefix.component.scss']
 })
 export class BikefixComponent {
-
+  auctionImages = [
+    { url: "/assets/bike/auction1.jpg", alt: "2022 bike auction 1" },
+    { url: "/assets/bike/auction2.jpg", alt: "2022 bike auction 2" },
+    { url: "/assets/bike/trek1.jpg", alt: "Trek hardtail 1" },
+    { url: "/assets/bike/trek2.jpg", alt: "Trek hardtail 2" },
+  ];
+  gatewayImages = [
+    { url: "/assets/bike/gateway1.jpg", alt: "Retro Trek navigator 2.0 hybrid bike" },
+    { url: "/assets/bike/gateway2.jpg", alt: "Kids' Diamondback Insight 24" },
+  ];
+  bikeImages = [
+    { url: "/assets/bike/bike1.jpg", alt: "Bicycle I flipped" },
+    { url: "/assets/bike/bike2.jpg", alt: "Bicycle I flipped" },
+    { url: "/assets/bike/bike3.jpg", alt: "Bicycle I flipped" },
+    { url: "/assets/bike/bike4.jpg", alt: "Bicycle I flipped" },
+    { url: "/assets/bike/bike5.jpg", alt: "Bicycle I flipped" },
+    { url: "/assets/bike/bike6.jpg", alt: "Bicycle I flipped" },
+    { url: "/assets/bike/bike7.jpg", alt: "Bicycle I flipped" },
+    { url: "/assets/bike/bike8.jpg", alt: "Bicycle I flipped" },
+    { url: "/assets/bike/bike9.jpg", alt: "Bicycle I flipped" },
+    { url: "/assets/bike/bike10.jpg", alt: "Bicycle I flipped" },
+    { url: "/assets/bike/bike11.jpg", alt: "Bicycle I flipped" },
+    { url: "/assets/bike/bike12.jpg", alt: "Bicycle I flipped" },
+    { url: "/assets/bike/bike13.jpg", alt: "Bicycle I flipped" },
+    { url: "/assets/bike/bike14.jpg", alt: "Bicycle I flipped" },
+    { url: "/assets/bike/bike15.jpg", alt: "Bicycle I flipped" },
+    { url: "/assets/bike/bike16.jpg", alt: "Bicycle I flipped" },
+    { url: "/assets/bike/bike17.jpg", alt: "Bicycle I flipped" },
+    { url: "/assets/bike/bike18.jpg", alt: "Bicycle I flipped" },
+    { url: "/assets/bike/bike19.jpg", alt: "Bicycle I flipped" },
+    { url: "/assets/bike/bike20.jpg", alt: "Bicycle I flipped" },
+    { url: "/assets/bike/bike21.jpg", alt: "Bicycle I flipped" },
+    { url: "/assets/bike/bike22.jpg", alt: "Bicycle I flipped" },
+    { url: "/assets/bike/bike23.jpg", alt: "Bicycle I flipped" },
+    { url: "/assets/bike/bike24.jpg", alt: "Bicycle I flipped" },
+    { url: "/assets/bike/bike25.jpg", alt: "Bicycle I flipped" },
+    { url: "/assets/bike/bike26.jpg", alt: "Bicycle I flipped" },
+    { url: "/assets/bike/bike27.jpg", alt: "Bicycle I flipped" },
+    { url: "/assets/bike/bike28.jpg", alt: "Bicycle I flipped" },
+    { url: "/assets/bike/bike29.jpg", alt: "Bicycle I flipped" },
+    { url: "/assets/bike/bike30.jpg", alt: "Bicycle I flipped" },
+    { url: "/assets/bike/bike31.jpg", alt: "Bicycle I flipped" },
+    { url: "/assets/bike/bike32.jpg", alt: "Bicycle I flipped" },
+    { url: "/assets/bike/bike33.jpg", alt: "Bicycle I flipped" }
+  ]
 }
