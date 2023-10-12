89cb07bca0072091b72d9c19121b4fdbe65fd990
gchuaunc <gchua@unc.edu>
2023-10-12 11:03:11 -0400

    fix: broken links for images

--- src/app/interests/programming/programming.component.ts
+++ src/app/interests/programming/programming.component.ts
@@ -7,26 +7,26 @@ import { Component } from '@angular/core';
 })
 export class ProgrammingComponent {
   cgixeosImages = [
-    { url: "/assets/programming/cgixeos1.png", alt: "Image 1 of cgixe os terminal program" },
-    { url: "/assets/programming/cgixeos2.png", alt: "Image 2 of cgixe os terminal program" },
-    { url: "/assets/programming/cgixeos3.png", alt: "Image 3 of cgixe os terminal program" },
-    { url: "/assets/programming/cgixeos4.png", alt: "Image 4 of cgixe os terminal program" }
+    { url: "./assets/programming/cgixeos1.png", alt: "Image 1 of cgixe os terminal program" },
+    { url: "./assets/programming/cgixeos2.png", alt: "Image 2 of cgixe os terminal program" },
+    { url: "./assets/programming/cgixeos3.png", alt: "Image 3 of cgixe os terminal program" },
+    { url: "./assets/programming/cgixeos4.png", alt: "Image 4 of cgixe os terminal program" }
   ];
   webImages = [
-    { url: "/assets/programming/web1.png", alt: "Image 1 of my web pages" },
-    { url: "/assets/programming/web2.png", alt: "Image 2 of my web pages" },
-    { url: "/assets/programming/web3.png", alt: "Image 3 of my web pages" },
-    { url: "/assets/programming/web4.png", alt: "Image 4 of my web pages" },
-    { url: "/assets/programming/web5.png", alt: "Image 5 of my web pages" },
-    { url: "/assets/programming/web6.png", alt: "Image 6 of my web pages" }
+    { url: "./assets/programming/web1.png", alt: "Image 1 of my web pages" },
+    { url: "./assets/programming/web2.png", alt: "Image 2 of my web pages" },
+    { url: "./assets/programming/web3.png", alt: "Image 3 of my web pages" },
+    { url: "./assets/programming/web4.png", alt: "Image 4 of my web pages" },
+    { url: "./assets/programming/web5.png", alt: "Image 5 of my web pages" },
+    { url: "./assets/programming/web6.png", alt: "Image 6 of my web pages" }
   ]
   unityImages = [
-    { url: "/assets/programming/unity1.png", alt: "Image 1 of my Unity games" },
-    { url: "/assets/programming/unity2.png", alt: "Image 2 of my Unity games" },
-    { url: "/assets/programming/unity3.png", alt: "Image 3 of my Unity games" },
-    { url: "/assets/programming/unity4.png", alt: "Image 4 of my Unity games" },
-    { url: "/assets/programming/unity5.png", alt: "Image 5 of my Unity games" },
-    { url: "/assets/programming/unity6.png", alt: "Image 6 of my Unity games" },
-    { url: "/assets/programming/unity7.png", alt: "Image 7 of my Unity games" },
+    { url: "./assets/programming/unity1.png", alt: "Image 1 of my Unity games" },
+    { url: "./assets/programming/unity2.png", alt: "Image 2 of my Unity games" },
+    { url: "./assets/programming/unity3.png", alt: "Image 3 of my Unity games" },
+    { url: "./assets/programming/unity4.png", alt: "Image 4 of my Unity games" },
+    { url: "./assets/programming/unity5.png", alt: "Image 5 of my Unity games" },
+    { url: "./assets/programming/unity6.png", alt: "Image 6 of my Unity games" },
+    { url: "./assets/programming/unity7.png", alt: "Image 7 of my Unity games" },
   ]
 }
